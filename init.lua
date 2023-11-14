3718e42a8eb0f42f75599f2496f0fd62dfae907e
anisphia <anisphia_wynn_palettia@proton.me>
2023-11-14 20:08:11 +0700

    add things from astrocommunity

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 return {
-  -- Configure AstroNvim updates
-  updater = {
+    updater = {
     remote = "origin", -- remote to use
     channel = "stable", -- "stable" or "nightly"
     version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
